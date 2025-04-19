27f3407ad780ecb2e074b133bea566318cbcfd4b
Danielle-design-art <daniellemorvant1@gmail.com>
2025-04-19 02:19:21 -0500

    added problems for labs

---
+++ part4-arrays.js
@@ -0,0 +1,64 @@
+console.log("arrays hello world");
+
+console.log("My Favorite Foods")
+
+let foods = ["Pizza", "Spaghetti", "Crawfish Boudin", "Chowder", "Hamburger", "Cake"];
+
+console.log(foods);
+
+console.log(foods.length);
+
+console.log(foods[0]);
+
+console.log(foods[foods.length - 1]);
+
+foods.push("Shrimp Fettucini");
+
+
+
+console.log("Guest List");
+
+let guests = ["Annette", "Brandy", "Louise", "Layla", "Kirby", "Ted", "Franklin", "Rebecca", "Gina"];
+
+console.log(guests)
+
+guests.shift();
+
+guests.push("Lisa")
+
+console.log(guests.length)
+
+
+
+console.log("Weekend Tasks")
+
+let tasks = [
+    "grocery shopping", 
+    "cut grass", 
+    "car maintenance", 
+    "laundry", 
+    "aquascaping", 
+    "Family Night"
+];
+
+console.log(tasks)
+
+for (let i = 0; i < tasks.length; i++) {
+    async function printTasks() {
+        //console.log(tasks[i]);
+    }
+  }
+
+ 
+ 
+  console.log("Reverse It")
+
+  let numbers = ["1","2","3","4","5"];
+
+console.log(numbers)
+
+  for (let i = numbers.length - 1; i > 0; i--) {
+    
+  }
+  
+
